0ab2c2321a3b66768138e2466bf0a6200d4cec24
Jake Diorio <jdiorio2393@gmail.com>
2020-07-23 12:49:19 -0400

    projects page changes

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -11,35 +11,21 @@ const projects = () => {
       </p>
       <div className='pic-container'>
         <div className='pic-div1'>
-          {/* <div className='text-div'> */}
           <div className='text-center text'>
             <h4>Stellar Ledgers</h4>
             <p>Accounting Firm</p>
           </div>
-          {/* </div> */}
-          {/* <img
-            className='work-pic'
-            id='stellar'
-            src='/projects/stellarledgers.png'
-            alt=''
-          ></img> */}
         </div>
         <div className='pic-div2'>
-          <div className='text-div'>
-            <h4>Stellar Ledgers</h4>
-            <p>Accounting Firm</p>
+          <div className='text-center text'>
+            <h4>Lookout</h4>
+            <p>Mobile Security</p>
           </div>
-          {/* <img
-            className='work-pic'
-            id='lookout'
-            src='/projects/lookout.png'
-            alt=''
-          ></img> */}
         </div>
         <div className='pic-div3'>
-          <div className='text-div'>
-            <h4>Stellar Ledgers</h4>
-            <p>Accounting Firm</p>
+          <div className='text-center text'>
+            <h4>Yellow Door</h4>
+            <p>Talent Development</p>
           </div>
           {/* <img
             className='work-pic'
@@ -49,9 +35,9 @@ const projects = () => {
           ></img> */}
         </div>
         <div className='pic-div4'>
-          <div className='text-div'>
-            <h4>Stellar Ledgers</h4>
-            <p>Accounting Firm</p>
+          <div className='text-center text'>
+            <h4>MediaKeys</h4>
+            <p>Media Agency</p>
           </div>
           {/* <img
             className='work-pic'
@@ -61,40 +47,42 @@ const projects = () => {
           ></img> */}
         </div>
         <div className='pic-div5'>
-          <div className='text-div'>
-            <h4>Stellar Ledgers</h4>
-            <p>Accounting Firm</p>
+          <div className='text-center text'>
+            <h4>Growing Me To Be</h4>
+            <p>Childrens Educational Institute</p>
           </div>
-          {/* <img
-            className='work-pic'
-            id='gmtb'
-            src='/projects/gmtb.png'
-            alt=''
-          ></img> */}
         </div>
         <div className='pic-div6'>
-          <div className='text-div'>
-            <h4>Stellar Ledgers</h4>
-            <p>Accounting Firm</p>
+          <div className='text-center text'>
+            <h4>SNR Creative</h4>
+            <p>Branding Firm</p>
           </div>
-          {/* <img
-            className='work-pic'
-            id='snr'
-            src='/projects/snrcreative.png'
-            alt=''
-          ></img> */}
         </div>
       </div>
       <h3 className='mobile-ordering'>Mobile Ordering</h3>
       <p className='work-description'>
-        A mobile ordering platform I Revamped via extensive CSS & HTML
+        A mobile ordering platform I revamped via extensive CSS & HTML
         templating without access to source code.
       </p>
-      <img className='work-pic' src='/projects/old.png' alt=''></img>
-      <h1 className='work-pic'>
-        <span style={{ color: '#0244fd' }}>{`=>`}</span>
-      </h1>
-      <img className='work-pic' src='/projects/new.png' alt=''></img>
+      <div className='pic-container2'>
+        <div className='pic-div7'>
+          <div className='text-center text'>
+            <h4>iMenuToGo</h4>
+            <p>Old Online Ordering</p>
+          </div>
+        </div>
+        {/* <img className='work-pic' src='/projects/old.png' alt=''></img> */}
+        <h1 className='work-pic'>
+          <span style={{ color: '#0244fd' }}>{`=>`}</span>
+        </h1>
+        <div className='pic-div8'>
+          <div className='text-center text'>
+            <h4>iMenuToGo</h4>
+            <p>New Online Ordering</p>
+          </div>
+        </div>
+        <img className='work-pic' src='/projects/new.png' alt=''></img>
+      </div>
     </div>
   );
 };
