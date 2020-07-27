ab68c62c8ad058c235498546fb64f849b99f2dcd
Jake Diorio <jdiorio2393@gmail.com>
2020-07-27 12:59:48 -0400

    sidebar responsiveness

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,51 @@
 import React, { Fragment } from 'react';
+import './sidebar.css';
 
 const Sidebar = () => {
   return (
     <Fragment>
-      <header className='header navbar-expand-md' role='banner'>
+      <nav className='menu' tabindex='0'>
+        <div className='smartphone-menu-trigger'></div>
+        <header className='avatar-profile'>
+          <img src='/portfolio-pic.jpg' />
+          <h2>Jake Diorio</h2>
+        </header>
+        <ul className=' unstyled list-hover-slide'>
+          <li tabindex='0'>
+            <a href='/'>
+              <i className='fas fa-home' aria-hidden='true'></i>
+              <p className='icon-text'>Home</p>
+            </a>
+          </li>
+          <li tabindex='0'>
+            <a href='/about'>
+              <i className='fas fa-user' aria-hidden='true'></i>
+              <p className='icon-text'>About</p>
+            </a>
+          </li>
+          <li tabindex='0'>
+            <a href='/my-work'>
+              <i className='fas fa-laptop-code' aria-hidden='true'></i>
+              <p className='icon-text'>Work</p>
+            </a>
+          </li>
+          <li tabindex='0'>
+            <a href='/contact'>
+              <i className='fas fa-envelope-open-text' aria-hidden='true'></i>
+              <p className='icon-text'>Contact</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* <header className='header navbar-expand-md' role='banner'>
         <div className='text-center'>
           <img className='profile-pic' src='/portfolio-pic.jpg' alt='' />
         </div>
-        <h1 className='logo'>
-          {/* eslint-disable-next-line */}
-          Jake Diorio
-        </h1>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#collapsibleNavbar'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
+        <h1 className='logo'>Jake Diorio</h1>
+        <a href='javascript:void(0)' className='closebtn' onclick='closeNav()'>
+          &times;
+        </a>
         <div className='nav-wrap'>
           <ul className='unstyled list-hover-slide'>
             <nav
@@ -26,29 +53,24 @@ const Sidebar = () => {
               role='navigation'
             >
               <li>
-                {/* eslint-disable-next-line */}
                 <a href='/'>
                   <i className='fas fa-home' aria-hidden='true'></i>
                   <p className='icon-text'>Home</p>
                 </a>
               </li>
               <li>
-                {/* eslint-disable-next-line */}
-
                 <a href='/about'>
                   <i className='fas fa-user' aria-hidden='true'></i>
                   <p className='icon-text'>About</p>
                 </a>
               </li>
               <li>
-                {/* eslint-disable-next-line */}
                 <a href='/my-work'>
                   <i className='fas fa-laptop-code' aria-hidden='true'></i>
                   <p className='icon-text'>Work</p>
                 </a>
               </li>
               <li>
-                {/* eslint-disable-next-line */}
                 <a href='/contact'>
                   <i
                     className='fas fa-envelope-open-text'
@@ -59,20 +81,11 @@ const Sidebar = () => {
               </li>
             </nav>
           </ul>
-          {/* <ul className='social-links  unstyled list-hover-slide text-center'>
-            <li>
-              
-              <img class='github-img' src='/github.png'></img>
-            </li>
-            <li>
-              
-              <a href='#'>Google+</a>
-            </li>
-          </ul> */}
         </div>
-      </header>
+      </header> */}
     </Fragment>
   );
+  // }
 };
 
 export default Sidebar;
